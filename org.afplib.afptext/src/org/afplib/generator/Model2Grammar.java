f85dd753bb3d7fddfe830dde06752ef80a610001
Yan Hackl-Feldbusch <yan@hcsystems.de>
2019-09-18 20:06:34 +0200

    initial commit

---
+++ org.afplib.afptext/src/org/afplib/generator/Model2Grammar.java
@@ -0,0 +1,159 @@
+package org.afplib.generator;
+
+import java.util.stream.Collectors;
+
+import org.afplib.afplib.AfplibPackage;
+import org.afplib.base.BasePackage;
+import org.eclipse.emf.ecore.EClass;
+
+public class Model2Grammar {
+
+	private static final EClass sfEClass = BasePackage.eINSTANCE.getSF();
+	private static final EClass tripletEClass = BasePackage.eINSTANCE.getTriplet();
+
+	public static void main(String[] args) {
+		
+		System.out.println("grammar org.afplib.AfpText with org.eclipse.xtext.common.Terminals\n" + 
+				"\n" + 
+				"generate afpText \"http://www.afplib.org/AfpText\"\n" + 
+				"\n" + 
+				"import \"http://www.eclipse.org/emf/2002/Ecore\" as ecore\n" + 
+				"\n" + 
+				"Model:\n" + 
+				"	(structuredFields+=structuredField)*;\n" + 
+				"\n" + 
+				"terminal MODCASTRING:\n" + 
+				"	'\"' ( '\\\\' . /* 'b'|'t'|'n'|'f'|'r'|'u'|'\"'|\"'\"|'\\\\' */ | !('\\\\'|'\"') )* '\"' |\n" + 
+				"	\"'\" ( '\\\\' . /* 'b'|'t'|'n'|'f'|'r'|'u'|'\"'|\"'\"|'\\\\' */ | !('\\\\'|\"'\") )* \"'\"\n" + 
+				";\n" + 
+				"\n" + 
+				"terminal EINTEGEROBJECT returns ecore::EInt: ('0'..'9')+;\n" + 
+				"\n" + 
+				"terminal EBYTEARRAY:\n" + 
+				"	('a'..'z'|'A'..'Z'|'0'..'9')*\n" + 
+				"	\n" + 
+				"	;\n" + 
+				"	\n" + 
+				"structuredField:");
+		
+		System.out.print("    (");
+		
+		System.out.println(AfplibPackage.eINSTANCE.getEClassifiers().stream()
+		.filter(EClass.class::isInstance)
+		.map(EClass.class::cast)
+		.filter(sfEClass::isSuperTypeOf)
+		.map(c -> c.getName())
+		.collect(Collectors.joining(" | ")));
+		
+		System.out.println(") '\\n';");
+		
+		AfplibPackage.eINSTANCE.getEClassifiers().stream()
+		.filter(EClass.class::isInstance)
+		.map(EClass.class::cast)
+		.filter(sfEClass::isSuperTypeOf)
+		.forEach(clazz -> {
+			System.out.println(clazz.getName()+":");
+			System.out.print("    {"+clazz.getName()+"} '"+clazz.getName()+"'");
+			clazz.getEAllAttributes().stream()
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getSF_Id()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getSF_Charset()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getSF_Children()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getSF_Length()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getSF_Number()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getSF_Offset()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getSF_RawData()))
+			.forEach(a -> {
+				System.out.print(" ('"+a.getName()+"=' ("+a.getName()+"=");
+				String typeName = a.getEType().getName().toUpperCase();
+				if("MODCASTRING4".equals(typeName)) typeName="MODCASTRING";
+				if("MODCASTRING8".equals(typeName)) typeName="MODCASTRING";
+				if("MODCASTRING32".equals(typeName)) typeName="MODCASTRING";
+				System.out.print(typeName+"))?");
+			});
+			clazz.getEAllContainments().stream()
+			.filter(c -> c.getName().equalsIgnoreCase("triplets"))
+			.forEach(c -> {
+				System.out.print(" (triplets+=triplet)*");
+			});
+			clazz.getEAllContainments().stream()
+			.filter(c -> c.getName().equalsIgnoreCase("rg"))
+			.forEach(c -> {
+				System.out.print(" (rg+="+clazz.getName()+"RG)*");
+			});
+			System.out.println(";");
+		});
+		
+		AfplibPackage.eINSTANCE.getEClassifiers().stream()
+		.filter(EClass.class::isInstance)
+		.map(EClass.class::cast)
+		.filter(tripletEClass::isSuperTypeOf)
+		.filter(clazz -> clazz.getName().endsWith("RG"))
+		.forEach(clazz -> {
+			System.out.println(clazz.getName()+":");
+			System.out.print("    {"+clazz.getName()+"} '\\n.rg'");
+			clazz.getEAllAttributes().stream()
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_Charset()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_FileOffset()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_Label()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_TripletId()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_TripletLength()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_TripletNumber()))
+			.forEach(a -> {
+				System.out.print(" ('"+a.getName()+"=' ("+a.getName()+"=");
+				String typeName = a.getEType().getName().toUpperCase();
+				if("MODCASTRING4".equals(typeName)) typeName="MODCASTRING";
+				if("MODCASTRING8".equals(typeName)) typeName="MODCASTRING";
+				if("MODCASTRING32".equals(typeName)) typeName="MODCASTRING";
+				System.out.print(typeName+"))?");
+			});
+			clazz.getEAllContainments().stream()
+			.filter(c -> c.getName().equalsIgnoreCase("triplets"))
+			.forEach(c -> {
+				System.out.print(" (triplets+=triplet)*");
+			});
+			System.out.println(";");
+		});
+		
+		System.out.println("triplet:");
+		System.out.print("    '\\n.' (");
+
+		System.out.println(AfplibPackage.eINSTANCE.getEClassifiers().stream()
+		.filter(EClass.class::isInstance)
+		.map(EClass.class::cast)
+		.filter(tripletEClass::isSuperTypeOf)
+		.filter(clazz -> !clazz.getName().endsWith("RG"))
+		.map(c -> c.getName())
+		.collect(Collectors.joining(" | ")));
+
+		System.out.println(");");
+
+		AfplibPackage.eINSTANCE.getEClassifiers().stream()
+		.filter(EClass.class::isInstance)
+		.map(EClass.class::cast)
+		.filter(tripletEClass::isSuperTypeOf)
+		.filter(clazz -> !clazz.getName().endsWith("RG"))
+		.forEach(clazz -> {
+			System.out.println(clazz.getName()+":");
+			System.out.print("    {"+clazz.getName()+"} '"+clazz.getName()+"'");
+			clazz.getEAllAttributes().stream()
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_Charset()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_FileOffset()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_Label()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_TripletId()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_TripletLength()))
+			.filter(a -> !a.equals(BasePackage.eINSTANCE.getTriplet_TripletNumber()))
+			.forEach(a -> {
+				System.out.print(" ('"+a.getName()+"=' ("+a.getName()+"=");
+				String typeName = a.getEType().getName().toUpperCase();
+				if("MODCASTRING4".equals(typeName)) typeName="MODCASTRING";
+				if("MODCASTRING8".equals(typeName)) typeName="MODCASTRING";
+				if("MODCASTRING32".equals(typeName)) typeName="MODCASTRING";
+				System.out.print(typeName+"))?");
+			});
+			System.out.println(";");
+		});
+		
+
+	}
+	
+}
---
+++ org.afplib.afptext/src/org/afplib/generator/Text2AFP.java
@@ -0,0 +1,120 @@
+package org.afplib.generator;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.afplib.afpText.structuredField;
+import org.afplib.afplib.AfplibFactory;
+import org.afplib.afplib.AfplibPackage;
+import org.afplib.base.BasePackage;
+import org.afplib.base.SF;
+import org.afplib.base.Triplet;
+import org.afplib.io.AfpOutputStream;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+
+public class Text2AFP {
+
+	private static final EClass sfEClass = BasePackage.eINSTANCE.getSF();
+	private static final EClass tripletEClass = BasePackage.eINSTANCE.getTriplet();
+	
+	private ByteArrayOutputStream result = new ByteArrayOutputStream();
+	private AfpOutputStream out;
+	
+	public Text2AFP() {
+		out = new AfpOutputStream(result);
+	}
+	
+	public void add(structuredField text) throws IOException {
+		System.out.println(text);
+		out.writeStructuredField(toSF(text));
+	}
+	
+	public byte[] get() {
+		return result.toByteArray();
+	}
+	
+	public static SF toSF(EObject text) {
+		String sfName = text.eClass().getName();
+		EClass sfClass = AfplibPackage.eINSTANCE.getEClassifiers().stream()
+			.filter(EClass.class::isInstance)
+			.map(EClass.class::cast)
+			.filter(sfEClass::isSuperTypeOf)
+			.filter(clazz -> clazz.getName().equals(sfName))
+			.findAny().orElse(null);
+		
+		SF sf = (SF) AfplibFactory.eINSTANCE.create(sfClass);
+		
+		copyAttributes(text, sf);
+		copyContainments(text, sf);
+
+		return sf;
+	}
+	
+	public static Triplet toTriplet(EObject text) {
+		String tName = text.eClass().getName();
+		EClass tClass = AfplibPackage.eINSTANCE.getEClassifiers().stream()
+			.filter(EClass.class::isInstance)
+			.map(EClass.class::cast)
+			.filter(tripletEClass::isSuperTypeOf)
+			.filter(clazz -> clazz.getName().equals(tName))
+			.findAny().orElse(null);
+		
+		Triplet triplet = (Triplet) AfplibFactory.eINSTANCE.create(tClass);
+		copyAttributes(text, triplet);
+		copyContainments(text, triplet);
+		
+		return triplet;
+	}
+
+	public static void copyAttributes(EObject text, EObject afp) {
+		for(EAttribute attr : text.eClass().getEAllAttributes()) {
+			afp.eClass().getEAllAttributes().stream()
+				.filter(a -> a.getName().equals(attr.getName()))
+				.findAny().ifPresent((a) -> {
+					Object value = text.eGet(attr);
+					if(value != null && a.getEType().getInstanceClass().equals(String.class)) {
+						value = ((String)value).substring(1, ((String)value).length() - 1); // remove "" or ''
+					}
+					if(value != null && a.getEType().getInstanceClass().equals(byte[].class)) {
+						value = DatatypeConverter.parseHexBinary((String) value);
+					}
+					afp.eSet(a, value);
+				});
+		}
+	}
+
+	public static void copyContainments(EObject text, EObject afp) {
+		
+		text.eClass().getEAllContainments().stream()
+			.filter(c -> c.getName().equalsIgnoreCase("triplets"))
+			.findAny().ifPresent(c -> {
+				List<EObject> triplets = (List<EObject>) text.eGet(c);
+				afp.eClass().getEAllContainments().stream()
+					.filter(c2 -> c2.getName().equalsIgnoreCase("triplets"))
+					.findAny().ifPresent(c2 -> {
+						List<EObject> afpTriplets = (List<EObject>) afp.eGet(c2);
+						triplets.stream().map(Text2AFP::toTriplet)
+							.forEach(afpTriplets::add);
+					});
+			});
+	
+		text.eClass().getEAllContainments().stream()
+			.filter(c -> c.getName().equalsIgnoreCase("rg"))
+				.forEach(c -> {
+					List<EObject> rg = (List<EObject>) text.eGet(c);
+					afp.eClass().getEAllContainments().stream()
+						.filter(c2 -> c2.getName().equalsIgnoreCase("rg"))
+						.findAny().ifPresent(c2 -> {
+							List<EObject> afpRG = (List<EObject>) afp.eGet(c2);
+							rg.stream().map(Text2AFP::toTriplet)
+								.forEach(afpRG::add);
+						});
+				});
+
+	}
+}
